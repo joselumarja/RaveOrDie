6265cf55c63f9b1354fd69b1cb75787729634347
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-08-02 16:20:04 +0200

    Principio playercontroller

---
+++ Source/RaveOrDie/RODPlayerController.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RODPlayerController.h"
+#include "AI/Navigation/NavigationSystem.h"
+#include "Runtime/Engine/Classes/Components/DecalComponent.h"
+#include "Engine/World.h"
+
+ARODPlayerController::ARODPlayerController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+}
+
+void ARODPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	// keep updating the destination every tick while desired
+	if (bMoveToMouseCursor)
+	{
+		MoveToMouseCursor();
+	}
+}
+
+void ARODPlayerController::SetupInputComponent()
+{
+	// set up gameplay key bindings
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("SetDestination", IE_Pressed, this, &ARODPlayerController::OnSetDestinationPressed);
+	InputComponent->BindAction("SetDestination", IE_Released, this, &ARODPlayerController::OnSetDestinationReleased);
+
+	// support touch devices 
+	InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ARODPlayerController::MoveToTouchLocation);
+	InputComponent->BindTouch(EInputEvent::IE_Repeat, this, &ARODPlayerController::MoveToTouchLocation);
+
+}
+
+
+void ARODPlayerController::MoveToMouseCursor()
+{
+
+
+		// Trace to see what is under the mouse cursor
+		FHitResult Hit;
+		GetHitResultUnderCursor(ECC_Visibility, false, Hit);
+
+		if (Hit.bBlockingHit)
+		{
+			// We hit something, move there
+			SetNewMoveDestination(Hit.ImpactPoint);
+		}
+	
+}
+
+void ARODPlayerController::MoveToTouchLocation(const ETouchIndex::Type FingerIndex, const FVector Location)
+{
+	FVector2D ScreenSpaceLocation(Location);
+
+	// Trace to see what is under the touch location
+	FHitResult HitResult;
+	GetHitResultAtScreenPosition(ScreenSpaceLocation, CurrentClickTraceChannel, true, HitResult);
+	if (HitResult.bBlockingHit)
+	{
+		// We hit something, move there
+		SetNewMoveDestination(HitResult.ImpactPoint);
+	}
+}
+
+void ARODPlayerController::SetNewMoveDestination(const FVector DestLocation)
+{
+	APawn* const MyPawn = GetPawn();
+	if (MyPawn)
+	{
+		UNavigationSystem* const NavSys = GetWorld()->GetNavigationSystem();
+		float const Distance = FVector::Dist(DestLocation, MyPawn->GetActorLocation());
+
+		// We need to issue move command only if far enough in order for walk animation to play correctly
+		if (NavSys && (Distance > 120.0f))
+		{
+			NavSys->SimpleMoveToLocation(this, DestLocation);
+		}
+	}
+}
+
+void ARODPlayerController::OnSetDestinationPressed()
+{
+	// set flag to keep updating destination until released
+	bMoveToMouseCursor = true;
+}
+
+void ARODPlayerController::OnSetDestinationReleased()
+{
+	// clear flag to indicate we should stop updating the destination
+	bMoveToMouseCursor = false;
+}
+
+
+
