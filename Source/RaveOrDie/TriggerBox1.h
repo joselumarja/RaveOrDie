6074a22cb5d1607700cb99b774c6667a4a3f99d1
Fabioballest <fabio.ballesteros@alu.uclm.es>
2018-08-19 19:05:14 +0200

    Añadida TriggerBox prueba

---
+++ Source/RaveOrDie/TriggerBox1.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "TriggerBox1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAVEORDIE_API ATriggerBox1 : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	ATriggerBox1();
+
+	UFUNCTION()
+		void OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor);
+
+	UFUNCTION()
+		void OnOverlapEnd(AActor* OverlappedActor, AActor* OtherActor);
+	
+	
+};
