97be487c139e03bb4db5da7dd39937c19f168210
Fabioballest <fabio.ballesteros@alu.uclm.es>
2018-09-01 15:55:02 +0200

    Cambios TriggerBox

--- Source/RaveOrDie/TriggerBox1.cpp
+++ Source/RaveOrDie/TriggerBox1.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TriggerBox1.h"
+#include "Enemigo.h"
+#include "MyAIController.h"
 #include "Engine.h"
 
 void ATriggerBox1::BeginPlay() {
@@ -12,12 +14,24 @@ ATriggerBox1::ATriggerBox1() {
 	OnActorBeginOverlap.AddDynamic(this, &ATriggerBox1::OnOverlapBegin);
 
 	OnActorEndOverlap.AddDynamic(this, &ATriggerBox1::OnOverlapEnd);
+
+
+	static ConstructorHelpers::FObjectFinder<UBlueprint> ItemBlueprint(TEXT("/Game/AI/Enemigo/BP_Enemigo.BP_Enemigo"));
+	if (ItemBlueprint.Object) {
+		MyItemBlueprint = (UClass*)ItemBlueprint.Object->GeneratedClass;
+	}
 }
 
 
 
 void ATriggerBox1::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor) {
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, TEXT("Dentro caja"));
+	
+	FVector RandomLocation(1500.f, 1000.f, 108.f);
+	FRotator Rotation(0.0f, 0.0f, 0.0f);
+
+	AEnemigo* DroppedItem = GetWorld()->SpawnActor<AEnemigo>(MyItemBlueprint, RandomLocation, Rotation);
+
 }
 void ATriggerBox1::OnOverlapEnd(AActor* OverlappedActor, AActor* OtherActor) {
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Black, TEXT("Fuera caja"));
