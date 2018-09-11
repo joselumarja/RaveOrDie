47a86bb9d9a0a890a4848ae61433063dbfe8c2a7
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-09-11 12:39:04 +0200

    Mapa

---
+++ Source/RaveOrDie/Boss.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemigo.h"
+#include "Boss.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAVEORDIE_API ABoss : public AEnemigo
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
