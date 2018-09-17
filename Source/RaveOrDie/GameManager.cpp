76b295e35b75b4f4a604b82563e8ff7717f487b1
Fabioballest <fabio.ballesteros@alu.uclm.es>
2018-09-17 19:53:09 +0200

    Mierdas del spawn

--- Source/RaveOrDie/GameManager.cpp
+++ Source/RaveOrDie/GameManager.cpp
@@ -3,6 +3,7 @@
 #include "GameManager.h"
 #include "Enemigo.h"
 #include "RangedEnemigo.h"
+#include "RODCharacter.h"
 #include "Boss.h"
 #include "MeleeEnemigo.h"
 #include "Runtime/Core/Public/Containers/Array.h"
@@ -12,7 +13,7 @@ TWeakObjectPtr<UGameManager> UGameManager::Manager = nullptr;
 
 UGameManager::UGameManager() 
 {
-	/*
+	
 	static ConstructorHelpers::FObjectFinder<UBlueprint> ItemBlueprint(TEXT("Game/AI/Enemigo/BP_MeleeEnemigo.BP_MeleeEnemigo"));
 	if (ItemBlueprint.Object) {
 		MyMeleeBlueprint = (UClass*)ItemBlueprint.Object->GeneratedClass;
@@ -28,7 +29,7 @@ UGameManager::UGameManager()
 	if (BossItemBlueprint.Object) {
 		MyBossBlueprint = (UClass*)BossItemBlueprint.Object->GeneratedClass;
 	}
-	*/
+	
 }
 
 TWeakObjectPtr<UGameManager> UGameManager::GetManager()
@@ -71,3 +72,41 @@ void UGameManager::GameOver()
 
 
 
+void UGameManager::SpawnEnemies(int Enemies) {
+	int32 aux;
+	for (int i = 0; i < Enemies; i++) {
+		FVector EnemySpawnLocation = GetRandomLocation();
+		FRotator Rotation(0.0f, 180.0f, 0.0f);
+		aux = GetRandomEnemyClass();
+		if (aux == 0){
+			AMeleeEnemigo* DroppedItem = GetWorld()->SpawnActor<AMeleeEnemigo>(MyMeleeBlueprint, EnemySpawnLocation, Rotation);
+			
+		}
+		else if (aux == 1) {
+			ARangedEnemigo* DroppedItem2 = GetWorld()->SpawnActor<ARangedEnemigo>(MyRangedBlueprint, EnemySpawnLocation, Rotation);
+		}
+
+		EnemiesAlived++;
+	}
+	
+}
+
+
+
+int32 UGameManager::GetRandomEnemyClass() const
+{
+	return FMath::RandRange(0, 2);
+}
+
+// Gets a random place to spawn an enemy
+FVector UGameManager::GetRandomLocation() const
+{
+	float x;
+	float y;
+	float z = PlayerPawn->GetActorLocation().Z;
+
+	y = FMath::RandRange(-1950, 1950);
+	x = FMath::RandRange(2900, 3000);
+	FVector RandomLocation(x, y, z);
+	return RandomLocation;
+}
