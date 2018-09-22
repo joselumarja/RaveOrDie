133e94158cf64fb36fb928b2dfec2a4759258e77
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-09-22 21:22:52 +0200

    Ajuste en las balas del enemigo

--- Source/RaveOrDie/Bullet.cpp
+++ Source/RaveOrDie/Bullet.cpp
@@ -7,11 +7,9 @@
 ABullet::ABullet()
 {
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("BulletMovement"));
-	ProjectileMovement->UpdatedComponent = ProjectileMesh;
 	ProjectileMovement->InitialSpeed = 800.f;
 	ProjectileMovement->MaxSpeed = 800.f;
-	ProjectileMovement->bShouldBounce = false;
-	ProjectileMovement->ProjectileGravityScale = 0.f; // No gravity
+	
 }
 
 void ABullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
--- Source/RaveOrDie/EnemyBullet.cpp
+++ Source/RaveOrDie/EnemyBullet.cpp
@@ -7,17 +7,21 @@
 
 AEnemyBullet::AEnemyBullet() : Super()
 {
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> ProjectileMeshAsset(TEXT("/Game/FPWeapon/Mesh/Crucifix.Crucifix"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ProjectileMeshAsset(TEXT("/Game/FPWeapon/Mesh/Amo/Cola.Cola"));
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet"));
 	ProjectileMesh->SetStaticMesh(ProjectileMeshAsset.Object);
 
 	ProjectileMesh->SetupAttachment(RootComponent);
 	ProjectileMesh->BodyInstance.SetCollisionProfileName("Bullet");
-
+	ProjectileMovement->UpdatedComponent = ProjectileMesh;
 	ProjectileMesh->OnComponentHit.AddDynamic(this, &ABullet::OnHit);		// set up a notification for when this component hits something
 	RootComponent = ProjectileMesh;
-
+	ProjectileMovement->ProjectileGravityScale = 0.8f; 
 	ProjectileMesh->bForceNavigationObstacle = true;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = true;
+
+	InitialLifeSpan = 1.5f;
 
 	Damage = 25;
 
